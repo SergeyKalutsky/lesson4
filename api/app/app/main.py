b37fe5638f230dc6d618ede3c0cbc07e4e45b308
Serget <skalytesky@gmail.com>
2021-04-20 21:26:43 +0300

    everything works kind of

--- api/app/app/main.py
+++ api/app/app/main.py
@@ -1,12 +1,13 @@
 import os
 import cv2
 import torch
+import requests
 import torchvision
 import numpy as np
 import torch.nn as nn
 from torchvision.models import resnet50
 import torchvision.transforms as transforms
-from flask import render_template, Flask, jsonify
+from flask import render_template, Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -41,6 +42,16 @@ def predict_img(img, model):
         return 'Cat'
 
 
+def save_img(src, filename):
+    path = f'/app/app/static/data/images/{filename}'
+    r = requests.get(src, stream=True)
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+    return path
+
+
 @app.route('/')
 def hello_world():
     return 'Hello world'
@@ -48,7 +59,11 @@ def hello_world():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    path = '/app/app/static/data/images/cat.jpg'
-    img = read_img_test(path)
-    res = predict_img(img, model)
-    return jsonify({'class_name': res})
+    if request.method == 'POST':
+        if 'url' in request.form and 'filename' in request.form:
+            path = save_img(request.form['url'], request.form['filename'])
+            img = read_img_test(path)
+            res = predict_img(img, model)
+            os.remove(path)
+            return jsonify({'class_name': res})
+    return jsonify({'key': 'data'})

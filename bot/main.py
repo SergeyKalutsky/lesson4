48255ba9d7a6c0feac2f46c2d2bbe861cc5a1c24
Serget <skalytesky@gmail.com>
2021-04-22 20:29:00 +0300

    fixed file arrangement

---
+++ bot/main.py
@@ -0,0 +1,59 @@
+# Рассказать что такое декораторы
+# Что такое api и с чем его едят
+import random
+import discord
+import requests
+
+
+TOKEN = 'ODMzNzgyODk1Mjk4MDg0OTI1.YH3W1Q.5rdyOxABV9TJs6Ap6tyadnjkykc'
+SERVER_NAME = 'Сергей МЛ'
+IMG_EXTENTIONS = ['jpg', 'png', 'jpeg']
+RULES = {'камень': 'бумага',
+         'ножницы': 'камень',
+         'бумага': 'ножницы'}
+
+client = discord.Client()
+
+
+def request_api_data(url, filename):
+    data = {"url": url, "filename": filename}
+    url = "http://104.236.235.5:5000/predict"
+    response = requests.post(url, data)
+    if response.status_code == 200:
+        label = response.json()['class_name']
+        return label
+    return 'Произошла ошибка при запросе данных'
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        print(guild)
+
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if len(message.attachments) == 1:
+        attachment = message.attachments[0]
+        ext = attachment.filename.split('.')[-1]
+        if ext in IMG_EXTENTIONS:
+            data = request_api_data(attachment.url, attachment.filename)
+            await message.channel.send(data)
+
+    if message.content.lower() in RULES.keys():
+        computer = random.choice(list(RULES.keys()))
+
+        await message.channel.send(computer)
+
+        if message.content.lower() == computer:
+            await message.channel.send('Ничья')    
+        elif message.content.lower() == RULES[computer]:
+            await message.channel.send('Ты победил')    
+        else:
+            await message.channel.send('Ты проиграл')   
+
+client.run(TOKEN)
